7259c0d78df842e376c7f78fa8c017702ee90fd8
Juliano Tavares <tavares@Julianos-MacBook-Pro.local>
2021-09-14 16:57:01 -0700

    import icons

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,11 +1,23 @@
 import React from 'react'
-
 import * as S from './styles'
 
 export function Dashboard() {
   return (
     <S.Container>
-      <S.Title>Dashboard</S.Title>
+      <S.Header>
+        <S.UserWrapper>
+          <S.UserInfo>
+            <S.Photo 
+              source={{ uri: 'https://avatars.githubusercontent.com/u/38234499?v=4' }}/>
+            <S.User>
+              <S.UserGreeting>Olá,</S.UserGreeting>
+              <S.UserName>Tavares</S.UserName>
+            </S.User>
+          </S.UserInfo>
+          <S.Icon name="power" />
+        </S.UserWrapper>
+
+      </S.Header>
     </S.Container>
   )
 }
